071e72ccc0e8780162815a18a7b41ad2bb175128
Andreas Kogler <andy.kogler.junior@gmail.com>
2021-04-30 16:03:45 +0200

    Remove boilerplate code

--- scenes/rocket.gd
+++ scenes/rocket.gd
@@ -2,12 +2,9 @@ extends RigidBody2D
 
 var acceleration = 5
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
-	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	processControls()
 
